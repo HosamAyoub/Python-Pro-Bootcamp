4eeaf65f6c59476ca896336fd600557f3dbd5162
Hosam Ayoub <hosamayoub50@gmail.com>
2022-08-29 19:54:33 +0200

    Update Day 2 Project Tip Calculator.py

--- Day 2 Project Tip Calculator.py
+++ Day 2 Project Tip Calculator.py
@@ -10,5 +10,6 @@ total_bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 total_bill *= (1 + (tip / 100))
-pay = round(total_bill / people, 2)
-print(f"Each person should pay: ${pay}")
+pay = total_bill / people
+final_bill = "{:.2f}".format(pay)
+print(f"Each person should pay: ${final_bill}")
