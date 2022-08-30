4d211fdea42cafbf896f0f7557ff67ac3897bd22
Hosam Ayoub <hosamayoub50@gmail.com>
2022-08-30 18:28:25 +0200

    Update Start and Exercise 3

--- 100 Days coding/Section 3 Day 3 - Beginner - Control Flow and Logical Operators/Day 3 Ecercise 1.py
+++ 100 Days coding/Section 3 Day 3 - Beginner - Control Flow and Logical Operators/Day 3 Ecercise 1.py
@@ -1,9 +1,23 @@
-# 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
-# 🚨 Don't change the code above 👆
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
 
-#Write your code below this line 👇
-if number % 2 == 0:
-  print("This is an even number.")
+if height >= 120:
+  print("You can ride the rollocoaster!")
+  age = int(input("How old are you? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7")
+  else:
+    bill = 12
+    print("Adult tickets are $12")
+
+  photo = input("Do you want a photo taken? Y or N.\n")
+  if photo == "Y" or photo == "y":
+    bill += 3
+  print(f"Your final bill ${bill}.")
 else:
-  print("This is an odd number.")
+  print("Sorry, you have to be taller before you can ride.")
