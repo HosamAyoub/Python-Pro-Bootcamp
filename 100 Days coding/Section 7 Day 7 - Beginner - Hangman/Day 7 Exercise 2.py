87008502e1d152929c2305bcb4bbd8cf08d1345c
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-03 12:59:25 +0200

    Update Day 7 Exercise 2.py

--- 100 Days coding/Section 7 Day 7 - Beginner - Hangman/Day 7 Exercise 2.py
+++ 100 Days coding/Section 7 Day 7 - Beginner - Hangman/Day 7 Exercise 2.py
@@ -17,11 +17,10 @@ for iterator in chosen_word:
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-index = 0
-for letter in chosen_word:
+for index in range(len(chosen_word)):
+    letter = chosen_word[index]
     if letter == guess:
         display[index] = guess
-    index += 1
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 print(display)
