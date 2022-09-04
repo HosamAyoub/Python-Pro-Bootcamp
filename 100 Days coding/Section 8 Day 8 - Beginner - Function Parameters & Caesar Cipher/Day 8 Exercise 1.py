ef9007534ba23f9ccd4035dc2fb84bf56d4d117a
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 21:36:06 +0200

    Create Day 8 Exercise 1.py

---
+++ 100 Days coding/Section 8 Day 8 - Beginner - Function Parameters & Caesar Cipher/Day 8 Exercise 1.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+def paint_calc(height, width,cover):
+    cans = (height * width) / cover
+    cans = ceil(cans)
+    print(f"You'll need {cans} cans of paint.")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
