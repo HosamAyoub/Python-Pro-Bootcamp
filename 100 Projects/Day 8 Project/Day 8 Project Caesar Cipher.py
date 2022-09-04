5ca14122bd61e472a0e104f2898fc4ae7538b4f7
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 23:49:43 +0200

    Update Day 8 Project Caesar Cipher.py

--- 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
+++ 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
@@ -34,7 +34,6 @@ def decrypt(plain_text, shift_amount):
 #plain_text = "hello"
 #print output: "The decoded text is hello"
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 if direction == 'encode':
     encrypt(text, shift)
 elif direction == 'decode':
