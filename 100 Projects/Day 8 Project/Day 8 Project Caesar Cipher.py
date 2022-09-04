8dc89ad4ea38f5dae78d8bb7e2e8f931ec4299e2
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 23:46:37 +0200

    Update Day 8 Project Caesar Cipher.py

--- 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
+++ 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
@@ -1,12 +1,14 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-decoded_word = ""
 
-def ecrypt(plain_text, shift_amount):
+def encrypt(plain_text, shift_amount):
     encrypted_word = ''
     for letter in plain_text:
         position = alphabet.index(letter)
@@ -15,10 +17,22 @@ def ecrypt(plain_text, shift_amount):
         else:
             encrypted_word += alphabet[position + shift_amount - len(alphabet)]
     print(f"The encoded text is {encrypted_word}")
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
 
-ecrypt(text, shift)
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text, shift_amount):
+    encrypted_word = ''
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        encrypted_word += alphabet[position - shift_amount]
+    print(f"The encoded text is {encrypted_word}")
+
+
+#e.g.
+#cipher_text = "mjqqt"
+#shift = 5
+#plain_text = "hello"
+#print output: "The decoded text is hello"
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+encrypt(text, shift)
