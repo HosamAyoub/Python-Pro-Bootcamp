884e0e2ac79b4cac2d13aad192600bebe6caf76c
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 23:57:52 +0200

    Update Day 8 Project Caesar Cipher.py

--- 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
+++ 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
@@ -5,29 +5,23 @@ alphabet = [
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n"))   
 
+def caesar(plain_text, shift_amount, code_direction):
+    if direction == 'encode':
+        encrypted_word = ''
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            if (position + shift_amount) < len(alphabet):
+                encrypted_word += alphabet[position + shift_amount]
+            else:
+                encrypted_word += alphabet[position + shift_amount - len(alphabet)]
+        print(f"The encoded text is {encrypted_word}")
+    elif direction == 'decode':
+        decoded_word = ""
+        for letter in plain_text:
+            position = alphabet.index(letter)
+            decoded_word += alphabet[position - shift_amount]
+        print(f"The decoded text is {decoded_word}")
 
-def encrypt(plain_text, shift_amount):
-    encrypted_word = ''
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        if (position + shift_amount) < len(alphabet):
-            encrypted_word += alphabet[position + shift_amount]
-        else:
-            encrypted_word += alphabet[position + shift_amount - len(alphabet)]
-    print(f"The encoded text is {encrypted_word}")
-
-
-def decrypt(plain_text, shift_amount):
-    decoded_word = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        decoded_word += alphabet[position - shift_amount]
-    print(f"The decoded text is {decoded_word}")
-
-
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
+caesar(text, shift, direction)
