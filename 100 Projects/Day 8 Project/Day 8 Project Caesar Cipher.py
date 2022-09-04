2acd2904617d2abb4bc28673903d77db1572f94e
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-05 01:12:40 +0200

    Update Day 8 Project Caesar Cipher.py

--- 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
+++ 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
@@ -1,3 +1,5 @@
+from art import logo
+
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
@@ -8,17 +10,26 @@ def caesar(plain_text, shift_amount, cipher_direction):
     if cipher_direction == 'decode': 
         shift_amount *= -1
     for letter in plain_text:
-        position = alphabet.index(letter)
-        if position + shift_amount < len(alphabet):
-            new_position = position + shift_amount
+        if letter.isalpha():
+            position = alphabet.index(letter)
+            if position + shift_amount < 26:
+                new_position = position + shift_amount
+            else:
+                new_position =  (position + shift_amount) % 26
+            end_text += alphabet[new_position]
         else:
-            new_position =  position + shift_amount - len(alphabet)
-        end_text += alphabet[new_position]
+            end_text += letter
     print(f"The {cipher_direction} text is {end_text}")
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))   
-
-
-caesar(text, shift, direction)
+print(logo)
+while(True):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if (direction == 'encode' or direction == 'decode'):
+        caesar(text, shift, direction)
+    else:
+        print("Please enter 'encode' or 'decode' next time.")
+    if input("Type 'no' to exit program.") == 'no':
+        print("Goodbye!")
+        break
