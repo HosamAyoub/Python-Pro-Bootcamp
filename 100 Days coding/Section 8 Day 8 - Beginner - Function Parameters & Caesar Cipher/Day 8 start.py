19a8f9bbe852ee6ebcbb5fbaf97b90ef6b821db7
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 17:00:35 +0200

    Create Day 8 start.py

---
+++ 100 Days coding/Section 8 Day 8 - Beginner - Function Parameters & Caesar Cipher/Day 8 start.py
@@ -0,0 +1,10 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet(name):
+    print("Hello,", end = " ")
+    print(name, end = "")
+    print("!")
+
+greet("Hosam")
