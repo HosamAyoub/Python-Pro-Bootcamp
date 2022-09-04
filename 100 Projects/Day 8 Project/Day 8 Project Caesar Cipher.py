856d3f96ea051ed05a2e1a1b6062be4bf39f76c7
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-05 00:33:11 +0200

    Update Day 8 Project Caesar Cipher.py

--- 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
+++ 100 Projects/Day 8 Project/Day 8 Project Caesar Cipher.py
@@ -3,25 +3,22 @@ alphabet = [
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+def caesar(plain_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == 'decode': 
+        shift_amount *= -1
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        if position + shift_amount < len(alphabet):
+            new_position = position + shift_amount
+        else:
+            new_position =  position + shift_amount - len(alphabet)
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction} text is {end_text}")
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))   
 
-def caesar(plain_text, shift_amount, cipher_direction):
-    if direction == 'encode':
-        encrypted_word = ''
-        for letter in plain_text:
-            position = alphabet.index(letter)
-            if (position + shift_amount) < len(alphabet):
-                encrypted_word += alphabet[position + shift_amount]
-            else:
-                encrypted_word += alphabet[position + shift_amount - len(alphabet)]
-        print(f"The encoded text is {encrypted_word}")
-    elif direction == 'decode':
-        decoded_word = ""
-        for letter in plain_text:
-            position = alphabet.index(letter)
-            decoded_word += alphabet[position - shift_amount]
-        print(f"The decoded text is {decoded_word}")
 
 caesar(text, shift, direction)
