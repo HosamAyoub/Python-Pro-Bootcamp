426577b0dbb109d460a8ecfa3aa2190777387b11
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-04 17:13:00 +0200

    Update Day 8 start.py

--- 100 Days coding/Section 8 Day 8 - Beginner - Function Parameters & Caesar Cipher/Day 8 start.py
+++ 100 Days coding/Section 8 Day 8 - Beginner - Function Parameters & Caesar Cipher/Day 8 start.py
@@ -1,6 +1,19 @@
-def greet():
-    print("Hello, Hosam")
-    print("How do you do?")
-    print("isn't the weather nice today?")
+# def greet():
+#     print("Hello, Hosam")
+#     print("How do you do?")
+#     print("isn't the weather nice today?")
+# greet()
 
-greet()
+#function with input
+# def greet_with_name(name):
+#     print(f"Hello, {name}")
+#     print("How do you do?")
+#     print("isn't the weather nice today?")
+
+# greet_with_name("Hosam")
+
+#functi with 2 inputs
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print(f"How do you do {name}?")
+    print("isn't the weather nice today?")
