3d855f4535b5e7c91f028e3dc5ccd971c6aa07f8
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-06 15:21:00 +0200

    Update Day 10 Exercise 1.py

--- 100 Days coding/Section 10 Day 10 - Beginner - Functions with Outputs/Day 10 Exercise 1.py
+++ 100 Days coding/Section 10 Day 10 - Beginner - Functions with Outputs/Day 10 Exercise 1.py
@@ -8,10 +8,11 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+    if 12 < month and month < 1:
+        return "Invalid month"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year):
-        month_days[1] = 29
-        return month_days[month - 1]
+    if is_leap(year) and month == 2:
+        return 29
     return month_days[month - 1]
     
   
