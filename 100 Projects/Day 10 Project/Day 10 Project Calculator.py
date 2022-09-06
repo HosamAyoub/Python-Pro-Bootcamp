b46bbc48a12658a5bf09819e2bddd1e2b157a63a
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-06 18:03:34 +0200

    Create Day 10 Project Calculator.py

---
+++ 100 Projects/Day 10 Project/Day 10 Project Calculator.py
@@ -0,0 +1,39 @@
+#Calculator
+#Add
+def add(n1, n2):
+    return n1 + n2
+#Subtract
+def subtact(n1, n2):
+    return n1 - n2
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtact,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    num1 = int(input("Enter the first number: "))
+    for operation in operations:
+        print(operation)
+    cont = True
+    while (cont):
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("Enter the next number: "))
+        operation_function = operations[operation_symbol]
+        result = operation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ") == "y":
+            num1 = result
+        else:
+            cont = False
+            calculator()
+
+calculator()
