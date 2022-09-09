d2105ef93dc04a92637dbbecfdd51c86735856c4
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-09 21:52:56 +0200

    Update Day 13 Exercise 3.py

--- 100 Days coding/Section 13 Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Day 13 Exercise 3.py
+++ 100 Days coding/Section 13 Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Day 13 Exercise 3.py
@@ -1,5 +1,5 @@
-for number in range(1, 101):
 #the first logic error her is because of (or) it should be (and) cause we need to print "FizzBuzz" only when the number is divisble by both 3 and 5 no just one of them
+# for number in range(1, 101):
   # if number % 3 == 0 or number % 5 == 0:
   #   print("FizzBuzz")
 #Second logic error here that just need to print "FizzBuzz" if the number was divisible by 3 and 5 not print "FizzBuzz" "Fizz" "Buzz" so we just need one if and others will be elif
@@ -11,12 +11,13 @@ for number in range(1, 101):
 #We need to print the number it self no list of the number
   #   print([number])
 
-  #Solution
+#Solution
+for number in range(1, 101):
   if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+  elif number % 5 == 0:
     print("Buzz")
   else:
     print(number)
