67848991a1fb7d0356250514f7cef3b0b1194810
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-09 21:36:40 +0200

    Create Day 13 Exercise 1.py

---
+++ 100 Days coding/Section 13 Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Day 13 Exercise 1.py
@@ -0,0 +1,7 @@
+number = int(input("Which number do you want to check?"))
+#The bug here is because of the assign operator it should be == not assign operator =
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
