2cffc2ea1565b960d15c709188a3bfbcbefe3993
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-10 23:30:03 +0200

    Update Day 14 Project Higher Lower Game.py

--- 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
+++ 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
@@ -3,6 +3,10 @@ from art import vs
 from game_data import data
 from random import choice
 
-def pick_two ():
-    first_choice = data.pick()
-    second_choice = data.pick()
+def pick ():
+    return choice(data)
+    
+
+print(logo)
+first_choice = pick()
+print(vs)
