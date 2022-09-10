6717eb471b6cfa9d92bfaab20536561ef265925a
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-10 23:47:37 +0200

    Update Day 14 Project Higher Lower Game.py

--- 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
+++ 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
@@ -15,3 +15,12 @@ second_choice = first_choice
 while(second_choice == first_choice):
     second_choice = pick()
 print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
+answer = None
+while ((answer != 'A') and (answer != 'B')) or ((answer != 'a') and (answer != 'b')):
+    answer = input("Who has more followers? Type 'A' or 'B': ")
+if (first_choice["follower_count"] > second_choice["follower_count"]):
+    right_answer = 'A'
+elif (first_choice["follower_count"] < second_choice["follower_count"]):
+    right_answer = 'B'
+else:
+    right_answer = "both"
