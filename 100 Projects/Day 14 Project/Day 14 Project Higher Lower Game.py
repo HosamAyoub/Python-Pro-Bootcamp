62fa5bdb62658de907d02e8e6d315730cf13a2e9
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-11 00:43:43 +0200

    Update Day 14 Project Higher Lower Game.py

--- 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
+++ 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
@@ -6,47 +6,51 @@ from replit import clear
 def pick ():
     return choice(data)
 
-def solution(choice):
+def solution(A, B):
     choice = ""
     while (choice.upper() != 'A') and (choice.upper() != 'B'):
         choice = input("Who has more followers? Type 'A' or 'B': ")
-    if (first_choice["follower_count"] > second_choice["follower_count"]):
+    if (A["follower_count"] > B["follower_count"]):
         right_choice = 'A'
-    elif (first_choice["follower_count"] < second_choice["follower_count"]):
+    elif (A["follower_count"] < B["follower_count"]):
         right_choice = 'B'
     else:
         right_choice = 'Both'
-    return right_choice
+    return choice, right_choice
 
-score = 0
-print(logo)
-first_choice = pick()
-print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
-print(vs)
-second_choice = first_choice
-
-while(second_choice == first_choice):
-    second_choice = pick()
-
-print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
-
-answer = ""
-while (answer.upper() != 'A') and (answer.upper() != 'B'):
-    answer = input("Who has more followers? Type 'A' or 'B': ")
-
-right_answer  = solution(answer)
-while ((answer.upper() == right_answer) or (right_answer == 'Both')):
-    clear()
-    score += 1
-    print(f"You're right! Current score: {score}")
-    first_choice = second_choice
+def game():
+    score = 0
+    print(logo)
+    first_choice = pick()
     print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
     print(vs)
+    second_choice = first_choice
     
     while(second_choice == first_choice):
         second_choice = pick()
-
+    
     print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
-    right_answer  = solution(answer)
+    
+    answer, right_answer  = solution(first_choice, second_choice)
+    
+    while ((answer.upper() == right_answer) or (right_answer == 'Both')):
+        clear()
+        score += 1
+        print(f"You're right! Current score: {score}")
+        first_choice = second_choice
+        print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
+        print(vs)
+        
+        while(second_choice == first_choice):
+            second_choice = pick()
+    
+        print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
+        answer, right_answer  = solution(first_choice, second_choice)
+    
+    print(f"Sorry, that's wrong. Final score: {score}")
 
-print(f"Sorry, that's wrong. Final score: {score}")
+while(True):
+    game()
+    if (input("Enter 'y' to play again or anythin to exit").upper() != "Y"):
+        print("Goodbye!")
+        break
