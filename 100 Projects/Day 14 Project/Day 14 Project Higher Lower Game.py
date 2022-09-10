806adf80d0001c12b53e90d99de125f9eab72ef4
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-10 23:56:15 +0200

    Update Day 14 Project Higher Lower Game.py

--- 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
+++ 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
@@ -7,6 +7,8 @@ def pick ():
     return choice(data)
     
 
+answer = None
+score = 0
 print(logo)
 first_choice = pick()
 print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
@@ -15,8 +17,7 @@ second_choice = first_choice
 while(second_choice == first_choice):
     second_choice = pick()
 print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
-answer = None
-score = 0
+
 while ((answer != 'A') and (answer != 'B')) or ((answer != 'a') and (answer != 'b')):
     answer = input("Who has more followers? Type 'A' or 'B': ")
 if (first_choice["follower_count"] > second_choice["follower_count"]):
@@ -28,5 +29,20 @@ else:
 while ((answer == right_answer) or (right_answer == 'Both')):
     score += 1
     print(f"You're right! Current score: {score}")
+    first_choice = second_choice
+    print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
+    print(vs)
+    second_choice = first_choice
+    while(second_choice == first_choice):
+        second_choice = pick()
+    print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
+    while ((answer != 'A') and (answer != 'B')) or ((answer != 'a') and (answer != 'b')):
+        answer = input("Who has more followers? Type 'A' or 'B': ")
+    if (first_choice["follower_count"] > second_choice["follower_count"]):
+        right_answer = 'A'
+    elif (first_choice["follower_count"] < second_choice["follower_count"]):
+        right_answer = 'B'
+    else:
+        right_answer = 'Both'
 else:
     print(f"Sorry, that's wrong. Final score: {score}")
