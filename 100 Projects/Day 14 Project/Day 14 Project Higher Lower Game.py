aeb0c15aeec2f560e5f22e80ee6676d40fa34984
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-10 23:42:50 +0200

    Update Day 14 Project Higher Lower Game.py

--- 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
+++ 100 Projects/Day 14 Project/Day 14 Project Higher Lower Game.py
@@ -11,3 +11,7 @@ print(logo)
 first_choice = pick()
 print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}.")
 print(vs)
+second_choice = first_choice
+while(second_choice == first_choice):
+    second_choice = pick()
+print(f"Compare B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}.")
