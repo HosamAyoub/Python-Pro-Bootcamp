c203deac49fd4e3c0d86e34fcbff79203965ecb4
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-14 03:51:17 +0200

    Update Day 16 Start.py

--- 100 Days coding/Section 16 Day 16 - Intermediate - Object Oriented Programming (OOP)/Day 16 Start.py
+++ 100 Days coding/Section 16 Day 16 - Intermediate - Object Oriented Programming (OOP)/Day 16 Start.py
@@ -1,12 +1,18 @@
-from turtle import Turtle, Screen
-import prettytable
+# from turtle import Turtle, Screen
 
-tom = Turtle()
-tom.shape("turtle")
-tom.color("dark blue")
-tom.forward(100)
-print(tom)
+# tom = Turtle()
+# tom.shape("turtle")
+# tom.color("dark blue")
+# tom.forward(100)
+# print(tom)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
