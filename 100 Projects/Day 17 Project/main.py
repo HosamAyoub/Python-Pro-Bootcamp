b71699f71dd01970d3d5f61a472f61591e8cd753
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-14 20:49:28 +0200

    Update main.py

--- 100 Projects/Day 17 Project/main.py
+++ 100 Projects/Day 17 Project/main.py
@@ -1,8 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for key in question_data:
     question_bank.append(Question(key['text'], key['answer']))
-print(question_bank)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
