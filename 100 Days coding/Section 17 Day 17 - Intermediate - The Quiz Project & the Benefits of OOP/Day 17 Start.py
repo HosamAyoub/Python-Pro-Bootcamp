cab3d9252bafb3b4822d8db56129aa9b31fc1815
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-14 20:07:51 +0200

    Update Day 17 Start.py

--- 100 Days coding/Section 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/Day 17 Start.py
+++ 100 Days coding/Section 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/Day 17 Start.py
@@ -2,10 +2,18 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
-user1 = User("007", "Hosam")
-print(user1.id)
-print(user1.username)
+user1 = User("001", "Hosam")
+user2 = User("002", "Ahmed")
+user2.follow(user1)
+print(f"{user1.username} has {user1.followers} followers and {user1.following} following")
+print(f"{user2.username} has {user2.followers} followers and {user2.following} following")
 
 # user1.id = "345"
 # user1.username = "Hosam"
