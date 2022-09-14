53feef018e1333c3a47842a6b44b2f784a2cab62
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-14 20:59:05 +0200

    Update quiz_brain.py

--- 100 Projects/Day 17 Project/quiz_brain.py
+++ 100 Projects/Day 17 Project/quiz_brain.py
@@ -4,9 +4,7 @@ class QuizBrain:
         self.question_list = question_list
     
     def still_has_questions(self):
-        if self.question_number < len(self.question_list):
-            return True
-        return False
+        return self.question_number < len(self.question_list)
         
     def next_question(self):
         input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False): ")
