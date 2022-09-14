27822ea788de0080085c6c5d456d423b146ab9af
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-14 18:24:22 +0200

    Create Day 17 Start.py

---
+++ 100 Days coding/Section 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/Day 17 Start.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
+user1 = User("007", "Hosam")
+print(user1.id)
+
+# user1.id = "345"
+# user1.username = "Hosam"
+# print(user1.username)
+
+# user2 = User()
+# user2.id = "345"
+# user2.username = "Hosam"
+# print(user2.username)
