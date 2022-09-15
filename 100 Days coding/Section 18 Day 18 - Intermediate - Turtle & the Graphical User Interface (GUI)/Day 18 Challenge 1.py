7c341b4e5be2e72ea081ab4d2a5c84ff3779c668
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-15 15:56:15 +0200

    Create Day 18 Challenge 1.py

---
+++ 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+tom = Turtle()
+tom.shape("turtle")
+tom.color("midnight blue")
+for _ in range(4):
+    tom.right(90)
+    tom.forward(100)
+
+screen_time = Screen()
+screen_time.exitonclick()
