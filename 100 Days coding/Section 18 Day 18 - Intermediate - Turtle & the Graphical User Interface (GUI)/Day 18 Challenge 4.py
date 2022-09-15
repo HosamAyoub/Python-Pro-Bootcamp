7a236e93596a2bb4d15246151564e0a39127fe76
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-15 16:54:21 +0200

    Update Day 18 Challenge 4.py

--- 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
+++ 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
@@ -2,12 +2,12 @@ from turtle import Turtle, Screen
 from random import choice, randint
 
 tom = Turtle()
-colors = ["black", "gray", "alice blue", "blue", "midnight blue", "cyan", "green", "dark green", "light green", "red", "orange", "purple", "brown"]
+colors = ["black", "gray", "light gray", "blue", "midnight blue", "cyan", "green", "dark green", "light green", "red", "orange", "purple", "brown"]
 tom.speed(6)
 tom.pensize(8)
 for sides in range(100):
     tom.color(choice(colors))
-    tom.right(90*randint(0, 3))
+    tom.setheading(90*randint(0, 3))
     tom.forward(100)
 
 screen_time = Screen()
