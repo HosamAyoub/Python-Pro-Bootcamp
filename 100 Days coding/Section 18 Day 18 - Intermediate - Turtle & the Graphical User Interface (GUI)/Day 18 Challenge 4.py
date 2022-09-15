fa9cd429a9cea540604cdfb51b8d934f19d40a5f
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-15 16:55:53 +0200

    Update Day 18 Challenge 4.py

--- 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
+++ 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
@@ -5,7 +5,7 @@ tom = Turtle()
 colors = ["black", "gray", "light gray", "blue", "midnight blue", "cyan", "green", "dark green", "light green", "red", "orange", "purple", "brown"]
 tom.speed(5)
 tom.pensize(10)
-for sides in range(100):
+for _ in range(100):
     tom.color(choice(colors))
     tom.setheading(90*randint(0, 3))
     tom.forward(100)
