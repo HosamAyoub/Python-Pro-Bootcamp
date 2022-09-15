4c23d343c049fe6ca3f1cf8cca9ab7998c3d597a
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-15 14:56:02 +0200

    Update Day 18 Start.py

--- 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Start.py
+++ 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Start.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 tom = Turtle()
 tom.shape("turtle")
 tom.color("midnight blue")
-for _ in range(0, 4):
+for _ in range(4):
     tom.right(90)
     tom.forward(100)
 
