ce53a8120a282cf881d9c1f12d2592bec6f27ffe
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-15 17:21:21 +0200

    Update Day 18 Challenge 4.py

--- 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
+++ 100 Days coding/Section 18 Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day 18 Challenge 4.py
@@ -1,12 +1,21 @@
 from turtle import Turtle, Screen
 from random import choice, randint
+import turtle
+
+def random_color():
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+    rgb = (red, green, blue)
+    return rgb
 
 tom = Turtle()
-colors = ["black", "gray", "light gray", "blue", "midnight blue", "cyan", "green", "dark green", "light green", "red", "orange", "purple", "brown"]
+# colors = ["black", "gray", "light gray", "blue", "midnight blue", "cyan", "green", "dark green", "light green", "red", "orange", "purple", "brown"]
+turtle.colormode(255)
 tom.speed(5)
 tom.pensize(10)
 for _ in range(100):
-    tom.color(choice(colors))
+    tom.color(random_color())
     tom.setheading(90*randint(0, 3))
     tom.forward(100)
 
