be9b85402f1c972a730172a47f2ef8f7a7a09d5a
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-16 22:28:56 +0200

    Update Day 13 Project Tic-Tac-Toe.py

--- 100 Projects/Day 13 Project/Day 13 Project Tic-Tac-Toe.py
+++ 100 Projects/Day 13 Project/Day 13 Project Tic-Tac-Toe.py
@@ -26,8 +26,7 @@ def make_list_of_free_fields(board):
 
 def victory_for(board, sign):
     winning_cases = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]
-    X = []
-    O = []
+    arr = []
     pos = 0
     if sign == 'X':
         for i in range(board.count("X")):
