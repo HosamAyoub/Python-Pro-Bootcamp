9a122bc2d76a2a0edf85a8ad96daad5b60d95ac3
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-16 22:12:41 +0200

    Update Day 13 Project Tic-Tac-Toe.py

--- 100 Projects/Day 13 Project/Day 13 Project Tic-Tac-Toe.py
+++ 100 Projects/Day 13 Project/Day 13 Project Tic-Tac-Toe.py
@@ -25,8 +25,49 @@ def make_list_of_free_fields(board):
 
 
 def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
+    winning_cases = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]
+    X = []
+    O = []
+    pos = 0
+    if sign == 'X':
+        for i in range(board.count("X")):
+            sub = 0
+            pos = board.index("X", pos+1)
+            if pos < 80:
+                while pos - sub - 56 > 3:
+                    sub += 7
+                X.append(pos - sub - 56)
+            elif pos < 180:
+                while pos - sub - 157 > 6:
+                    sub += 7
+                X.append(pos - sub - 157)
+            elif pos < 300:
+                while pos - sub - 258 > 9:
+                    sub += 7
+                X.append(pos - sub - 258)
+        if X in winning_cases:
+            print("You lose🥱 ")
+            return True
+    elif sign == 'O':
+        for i in range(board.count("O")):
+            sub = 0
+            pos = board.index("O", pos+1)
+            if pos < 80:
+                while pos - sub - 56 > 3:
+                    sub += 7
+                O.append(pos - sub - 56)
+            elif pos < 180:
+                while pos - sub - 157 > 6:
+                    sub += 7
+                O.append(pos - sub - 157)
+            elif pos < 300:
+                while pos - sub - 258 > 9:
+                    sub += 7
+                O.append(pos - sub - 258)
+        if O in winning_cases:
+            print("You win😎 ")
+            return True
+    return False
 
 
 def draw_move(board):
