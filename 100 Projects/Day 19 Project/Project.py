88da6e1e45e3774405b9c64885e4d85298b0c464
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-18 00:47:06 +0200

    Update Project.py

--- 100 Projects/Day 19 Project/Project.py
+++ 100 Projects/Day 19 Project/Project.py
@@ -2,18 +2,22 @@ from turtle import Turtle, Screen
 
 donatello = Turtle(shape = "turtle")
 donatello.penup()
+donatello.color("purole")
 
 leonardo = Turtle(shape = "turtle")
 leonardo.penup()
+leonardo.color("blue")
 
 michelangelo = Turtle(shape = "turtle")
 michelangelo.penup()
+michelangelo.color("orange")
 
 raphael = Turtle(shape = "turtle")
 raphael.penup()
+raphael.color("red")
 
 screen = Screen()
-screen.setup(width = 500, height = 400)
+screen.setup(width = 500, height = 450)
 user_bet = screen.textinput(title = "Make your bet", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
 
 donatello.goto(x = -235, y = 150)
