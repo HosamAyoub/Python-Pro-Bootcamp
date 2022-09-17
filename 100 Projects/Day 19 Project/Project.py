a3183d0687b0602263a515937a0938022601e7c3
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-18 00:31:19 +0200

    Create Project.py

---
+++ 100 Projects/Day 19 Project/Project.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+tom = Turtle()
+screen = Screen()
+screen.setup(width = 500, height = 400)
+screeen.textinput(title = "Make your bet.", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ")
+
+
+screen.exitonclick()
