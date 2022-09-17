0f58d93a71991a44622663a3b882567275d77b6b
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-18 01:14:25 +0200

    Update Project.py

--- 100 Projects/Day 19 Project/Project.py
+++ 100 Projects/Day 19 Project/Project.py
@@ -8,6 +8,7 @@ turtles_names = ["donatello", "leonardo", "michelangelo", "raphael"]
 screen = Screen()
 screen.setup(width = 500, height = 450)
 user_bet = ""
+
 while user_bet not in turtles_names:
     user_bet = screen.textinput(title = "Make your bet", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
 
@@ -17,5 +18,16 @@ for turtle_index in range(4):
     new_turtle.color(colors[turtle_index])
     new_turtle.goto(x = -235, y = y_positions[turtle_index])
     turtles.append(new_turtle)
+
+while True:
+
+    for turtle in turtles:
+        turtle.forward(randint(0, 10))
+        if turtle.xcor() >= 235:
+            if turtles.index(turtle) == turtles_names.index(user_bet):
+                print(f"You've won! the {user_bet.capitalize()} turtle is the winner!")
+            else:
+                print(f"You've lost! the {turtles_names[turtles.index(turtle)].capitalize()} turtle is the winner!")
+            break
 
 screen.exitonclick()
