b8febde7544a4bbc185d7be8eadefb17fb0b683e
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-18 00:35:32 +0200

    Update Project.py

--- 100 Projects/Day 19 Project/Project.py
+++ 100 Projects/Day 19 Project/Project.py
@@ -3,6 +3,6 @@ from turtle import Turtle, Screen
 tom = Turtle()
 screen = Screen()
 screen.setup(width = 500, height = 400)
-user_bet = screen.textinput(title = "Make your bet.", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
+user_bet = screen.textinput(title = "Make your bet", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
 
 screen.exitonclick()
