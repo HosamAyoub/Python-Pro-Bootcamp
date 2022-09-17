6ac9267fef6b3e44760c894a7bccbc63866b226d
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-18 01:04:05 +0200

    Update Project.py

--- 100 Projects/Day 19 Project/Project.py
+++ 100 Projects/Day 19 Project/Project.py
@@ -4,10 +4,12 @@ from random import randint
 colors = ["purple", "blue", "orange", "red"]
 y_positions = [150, 50, -50, -150]
 turtles = []
-
+turtles_names = ["Donatello", "Leonardo", "Michelangelo", "Raphael"]
 screen = Screen()
 screen.setup(width = 500, height = 450)
-user_bet = screen.textinput(title = "Make your bet", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
+user_bet = ""
+while user_bet not in turtles_names:
+    user_bet = screen.textinput(title = "Make your bet", prompt = "Choose between \"Donatello\", \"Leonardo\", \"Michelangelo\", \"Raphael\"\nEnter your choice: ").lower()
 
 for turtle_index in range(4):
     new_turtle = Turtle(shape = "turtle")
