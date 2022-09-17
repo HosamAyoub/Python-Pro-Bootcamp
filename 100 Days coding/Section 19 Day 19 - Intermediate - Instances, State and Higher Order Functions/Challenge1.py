69f9e177316cb40e1f647aeec5398b988a30aabc
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-17 23:17:09 +0200

    Update Challenge1.py

--- 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Challenge1.py
+++ 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Challenge1.py
@@ -11,9 +11,13 @@ def move_backward():
 
 def rotate_clock_wise():
     tom.right(10)
+    
+def rotate_counter_clock_wise():
+    tom.left(10)
 
 screen.listen()
 screen.onkey(key = "w", fun = move_forward)
 screen.onkey(key = "s", fun = move_backward)
 screen.onkey(key = "d", fun = rotate_clock_wise)
+screen.onkey(key = "a", fun = rotate_counter_clock_wise)
 screen.exitonclick()
