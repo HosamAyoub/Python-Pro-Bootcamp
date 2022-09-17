ecb130394157be1ef1dbac68484119eb39ed00c3
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-17 23:14:10 +0200

    Update Challenge1.py

--- 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Challenge1.py
+++ 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Challenge1.py
@@ -6,6 +6,10 @@ screen = Screen()
 def move_forward():
     tom.forward(10)
 
+def move_backward():
+    tom.bakcward(10)
+
 screen.listen()
-screen.onkey(key = "space", fun = move_forward)
+screen.onkey(key = "w", fun = move_forward)
+screen.onkey(key = "s", fun = move_backward)
 screen.exitonclick()
