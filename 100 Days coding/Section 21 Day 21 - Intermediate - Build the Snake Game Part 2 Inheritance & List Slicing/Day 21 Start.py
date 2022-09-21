fc119ee254165dd8c7d923ea9f7d4ead8e08234c
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-22 00:33:38 +0200

    Update Day 21 Start.py

--- 100 Days coding/Section 21 Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Day 21 Start.py
+++ 100 Days coding/Section 21 Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Day 21 Start.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,5 +26,6 @@ while True:
     time.sleep(0.1)
     snake.move()
     if snake.head.distance(food) < 15:
+        scoreboard.increment_score()
         food.refresh()
     
