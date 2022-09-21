60b655e927063333383aa0986156bcf782a568dd
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-22 01:04:12 +0200

    Create scoreboard.py

---
+++ 100 Projects/Day 21 Project/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+FONT = ('Arial', 12, 'normal')
+ALIGN = 'center'
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.create_score_board()
+    
+    def create_score_board(self):
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 280)
+        self.color('white')
+        self.write(f"score: {self.score}", move=False, align=ALIGN, font=(FONT))
+        
+    def increment_score(self):
+        self.score += 1
+        self.clear()
+        self.create_score_board()
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over.", move=False, align=ALIGN, font=(FONT))
+        
