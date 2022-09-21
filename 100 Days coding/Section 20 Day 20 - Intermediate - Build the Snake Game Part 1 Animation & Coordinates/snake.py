475d9c89b47927bf9219d9c6d3c79dfcc0f6cde5
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-21 23:00:44 +0200

    Update snake.py

--- 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
+++ 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 X_STARTING_POSITIONS = [0, -20, -40]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -27,20 +31,20 @@ class Snake:
     
     
     def up(self):
-        if self.head.heading() != 270:
-            self.head.seth(90)
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
     
     
     def down(self):
-        if self.head.heading() != 90:
-            self.head.seth(270)
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
     
     
     def right(self):
-        if self.head.heading() != 180:
-            self.head.seth(0)
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
         
     
     def left(self):
-        if self.head.heading() != 0:
-            self.head.seth(180)
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
