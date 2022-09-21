da47845aa4844a037a310f6962eaf679db3757ae
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-21 19:11:40 +0200

    Update Day 20 Start.py

--- 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/Day 20 Start.py
+++ 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/Day 20 Start.py
@@ -1,17 +1,25 @@
+import imp
 from turtle import Turtle, Screen
-import turtle
+import time
 
 screen = Screen()
-screen.setup(width = 600, height = 600)
-screen.bgcolor('black')
+screen.setup(width=600, height=600)
 screen.title('Snake Game')
-x_positions = [-40, -20, 0]
-turtles = []
-for x_pos in x_positions:
-    turtle = Turtle(shape = 'square')
-    turtle.color('white')
-    turtle.penup()
-    turtle.goto(x = x_pos, y = 0)
-    turtles.append(turtle)
+screen.bgcolor('black')
+screen.tracer(0)
 
-screen.exitonclick()
+
+turtles = []
+x_positions = [-40, -20, 0]
+for x_pos in x_positions:
+    new_turtle = Turtle(shape='square')
+    new_turtle.penup()
+    new_turtle.color('white')
+    new_turtle.goto(x=x_pos, y=0)
+    turtles.append(new_turtle)
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+    for turtle in turtles:
+        turtle.forward(20)
