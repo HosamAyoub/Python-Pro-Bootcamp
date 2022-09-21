2173b40c64003d843619bbb0ee32a07c4686a063
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-21 20:04:24 +0200

    Update snake.py

--- 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
+++ 100 Days coding/Section 20 Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
@@ -1,15 +1,21 @@
 from turtle import Turtle
 
+X_STARTING_POSITIONS = [0, -20, -40]
+
 class Snake:
     def __init__(self):
         self.turtles = []
-        self.x_positions = [0, -20, -40]
-        for x_pos in self.x_positions:
+        self.create_snake()
+    
+    
+    def create_snake(self):
+        for x_pos in X_STARTING_POSITIONS:
             new_turtle = Turtle(shape='square')
             new_turtle.penup()
             new_turtle.color('white')
             new_turtle.goto(x=x_pos, y=0)
             self.turtles.append(new_turtle)
+
 
     def move(self):
         for turtle in range(len(self.turtles) - 1, 0, -1):
