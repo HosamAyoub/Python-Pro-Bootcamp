e4d3d000005353dfa6177a315badab4bde1442e4
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-22 01:04:47 +0200

    Update Day 21 Start.py

--- 100 Days coding/Section 21 Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Day 21 Start.py
+++ 100 Days coding/Section 21 Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/Day 21 Start.py
@@ -21,11 +21,27 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
-while True:
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    
+    #Detect collision with food
     if snake.head.distance(food) < 15:
         scoreboard.increment_score()
+        snake.extend()
         food.refresh()
     
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+    
+    #Detect collision with tail
+    for segment in snake.segments[3:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
+    
+screen.exitonclick()
