12a38b3eb93950c9e382baef8c05694625623593
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-27 00:09:54 +0200

    Update Day 22 Start.py

--- 100 Days coding/Section 22 Day 22 - Intermediate - Build Pong The Famous Arcade Game/Day 22 Start.py
+++ 100 Days coding/Section 22 Day 22 - Intermediate - Build Pong The Famous Arcade Game/Day 22 Start.py
@@ -13,17 +13,25 @@ right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(right_paddle.up, "Up")
-screen.onkey(right_paddle.down, "Down")
-screen.onkey(left_paddle.up, "w")
-screen.onkey(left_paddle.down, "s")
+screen.onkeypress(right_paddle.up, "Up")
+screen.onkeypress(right_paddle.down, "Down")
+screen.onkeypress(left_paddle.up, "w")
+screen.onkeypress(left_paddle.down, "s")
 
 ball = Ball()
 
 while True:
+    time.sleep(0.09)
     screen.update()
-    ball.move_ball()
-    ball.collision_with_wall()
-    time.sleep(0.07)
+    ball.move()
+    #Detect collision with wall
+    if -280 > ball.ycor() or ball.ycor() > 280:
+        ball.y_bounce()
+    #Detect collision with paddle
+    if (ball.distance(right_paddle) < 50 and ball.xcor() > 325) or (ball.distance(left_paddle) < 50 and ball.xcor() < -325):
+        ball.x_bounce()
+    #Detect when the paddle misses
+    if ball.xcor() > 420 or ball.xcor() < -420:
+        ball.reset()
 
 screen.exitonclick()
