4d75c8b77e6c2cd54c6449929d8ad8b365b5acc7
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-26 20:27:45 +0200

    Create paddle.py

---
+++ 100 Days coding/Section 22 Day 22 - Intermediate - Build Pong The Famous Arcade Game/paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+NORTH = 90
+STEP = 20
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddle(position)
+    
+    def create_paddle(self, position):
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.color('White')
+        self.seth(NORTH)
+        self.goto(position)
+    
+    def up(self):
+        self.fd(STEP)
+        
+    def down(self):
+        self.bk(STEP)
