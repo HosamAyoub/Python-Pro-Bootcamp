885a13eaff5663aaca50a4fd3d3ad6eea0548cc5
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-26 20:26:45 +0200

    Update Day 22 Start.py

--- 100 Days coding/Section 22 Day 22 - Intermediate - Build Pong The Famous Arcade Game/Day 22 Start.py
+++ 100 Days coding/Section 22 Day 22 - Intermediate - Build Pong The Famous Arcade Game/Day 22 Start.py
@@ -6,3 +6,11 @@ screen.tracer(0)
 screen.bgcolor('black')
 screen.title('Ping Pong')
 screen.setup(width=800, height=600)
+
+
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
