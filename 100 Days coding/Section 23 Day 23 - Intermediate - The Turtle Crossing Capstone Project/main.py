75e4fc00492f5f256cbe7eef463cd1d5c304160b
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-27 04:39:29 +0200

    Update main.py

--- 100 Days coding/Section 23 Day 23 - Intermediate - The Turtle Crossing Capstone Project/main.py
+++ 100 Days coding/Section 23 Day 23 - Intermediate - The Turtle Crossing Capstone Project/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    if player.finish():
+        scoreboard.next_level()
     screen.update()
