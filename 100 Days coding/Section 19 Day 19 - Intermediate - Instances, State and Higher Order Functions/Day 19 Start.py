e388cc849f8dd8732d81f903021ec51de6cf38f6
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-27 06:22:54 +0200

    Update Day 19 Start.py

--- 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Day 19 Start.py
+++ 100 Days coding/Section 19 Day 19 - Intermediate - Instances, State and Higher Order Functions/Day 19 Start.py
@@ -7,5 +7,5 @@ def move_forward():
     tom.forward(10)
 
 screen.listen()
-screen.onkey(key = "space", fun = move_forward)
+screen.onkeypress(key = "space", fun = move_forward)
 screen.exitonclick()
