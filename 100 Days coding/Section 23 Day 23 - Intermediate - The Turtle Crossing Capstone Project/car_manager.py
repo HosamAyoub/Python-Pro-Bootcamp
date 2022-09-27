afb9f7033d70c29dbdf3aa213dbc93c998be8368
Hosam Ayoub <hosamayoub50@gmail.com>
2022-09-27 06:23:21 +0200

    Update car_manager.py

--- 100 Days coding/Section 23 Day 23 - Intermediate - The Turtle Crossing Capstone Project/car_manager.py
+++ 100 Days coding/Section 23 Day 23 - Intermediate - The Turtle Crossing Capstone Project/car_manager.py
@@ -3,13 +3,14 @@ from secrets import choice
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+STARTING_MOVE_DISTANCE = 4
+MOVE_INCREMENT = 2
 
 
 class CarManager():
     def __init__(self):
         self.turtles = []
+        self.speed = STARTING_MOVE_DISTANCE
         
     def create(self):
         for _ in range(100):
@@ -18,11 +19,14 @@ class CarManager():
             new_turtle.shape('square')
             new_turtle.color(choice(COLORS))
             new_turtle.shapesize(stretch_len=2, stretch_wid=1)
-            new_turtle.goto(randint(200, 2000), randint(-250, 280))
+            new_turtle.goto(randint(-200, 2000), randint(-250, 280))
             self.turtles.append(new_turtle)
     
-    def move(self, level):
+    def move(self):
         for t in self.turtles:
-            t.goto(t.xcor() - STARTING_MOVE_DISTANCE - ((level - 1) * MOVE_INCREMENT), t.ycor())
+            t.backward(self.speed)
             if t.xcor() < -350:
-                t.goto(randint(350, 2000), t.ycor())
+                t.goto(randint(250, 2000), randint(-250, 280))
+            
+    def speed_up(self):
+        self.speed += MOVE_INCREMENT
