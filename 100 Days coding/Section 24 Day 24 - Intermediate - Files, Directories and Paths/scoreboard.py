42f6218c37aec0e4f391f476fa384e04a8c7a3f6
Hosam Ayoub <hosamayoub50@gmail.com>
2022-10-04 23:03:23 +0200

    Update scoreboard.py

--- 100 Days coding/Section 24 Day 24 - Intermediate - Files, Directories and Paths/scoreboard.py
+++ 100 Days coding/Section 24 Day 24 - Intermediate - Files, Directories and Paths/scoreboard.py
@@ -6,15 +6,25 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        try:
+            with open("data.txt", "r") as file:
+                self.high_score = int(file.read())
+        except:
+            with open("data.txt", "w") as file:
+                self.high_score = 0
+                file.write(str(self.high_score))
         self.create_score_board()
     
     def create_score_board(self):
-        self.clear()
         self.penup()
         self.hideturtle()
         self.goto(0, 280)
         self.color('white')
+        self.update_score_board()
+        
+    
+    def update_score_board(self):
+        self.clear()
         self.write(f"score: {self.score}  |  High score: {self.high_score}", move=False, align=ALIGN, font=(FONT))
         
     def increment_score(self):
@@ -24,5 +34,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
+        self.update_score_board()
         
