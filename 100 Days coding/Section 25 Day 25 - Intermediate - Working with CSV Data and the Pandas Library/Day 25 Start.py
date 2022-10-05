f163c56b050bb41a8e430ca9e33dbdd5cdbcdf06
Hosam Ayoub <hosamayoub50@gmail.com>
2022-10-05 21:02:48 +0200

    Create Day 25 Start.py

---
+++ 100 Days coding/Section 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day 25 Start.py
@@ -0,0 +1,35 @@
+# with open ('weather_data.csv') as file:
+#     data = file.readlines()  
+#     print(data)
+
+# import csv
+
+# with open ('weather_data.csv') as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#         print(row)
+#         print(temperatures)
+
+import pandas
+
+# This is a data frame
+data = pandas.read_csv('weather_data.csv')
+print(data)
+print()
+# data['temp'] is a series
+print(data['temp'])
+print()
+# converting data frame to a dictionary
+data_dict = data.to_dict()
+print(data_dict)
+print()
+# converting series to a list
+data_list = data['temp'].to_list()
+print(data_list)
+
+# get average of the temps
+print(f'{sum(data_list)/len(data_list):.2f}')
+print(f'{data["temp"].mean():.2f}')
