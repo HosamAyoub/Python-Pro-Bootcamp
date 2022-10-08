9c271094cfef0fb6e2f225f698588d4b054870bf
Hosam Ayoub <hosamayoub50@gmail.com>
2022-10-08 17:43:52 +0200

    Update Day 25 Start.py

--- 100 Days coding/Section 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day 25 Start.py
+++ 100 Days coding/Section 25 Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day 25 Start.py
@@ -11,7 +11,7 @@
 #         if row[1] != 'temp':
 #             temperatures.append(int(row[1]))
 #         print(row)
-#         print(temperatures)
+#     print(temperatures)
 
 import pandas
 
@@ -33,3 +33,26 @@ print(data_list)
 # get average of the temps
 print(f'{sum(data_list)/len(data_list):.2f}')
 print(f'{data["temp"].mean():.2f}')
+print(data["temp"].max())
+
+# get data from column
+print(data['day'])
+print(data.day)
+
+# get data from row
+print(data[data.day == 'Tuesday'])
+
+# get the row of data which has the maximum temperature of the week
+print(data[data.temp == data.temp.max()])
+
+hot_day = data[data.temp == data.temp.max()]
+print((hot_day.temp * 9/5) + 32)
+
+# create data frame from scratch
+new_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 32, 54]
+}
+data = pandas.DataFrame(new_dict)
+print(data)
+data.to_csv('new_data.csv')
