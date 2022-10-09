8f21281128851fe567d98afcc5775e7aaec88026
Hosam Ayoub <hosamayoub50@gmail.com>
2022-10-09 03:51:02 +0200

    Update main.py

--- 100 Projects/Day 25 Egypt States Game/main.py
+++ 100 Projects/Day 25 Egypt States Game/main.py
@@ -4,7 +4,7 @@ import pandas
 screen = Screen()
 screen.title('Egypt States Game')
 screen.setup(731, 674)
-image = r'Egypt_blank_states.gif'
+image = 'Egypt_blank_states.gif'
 screen.addshape(image)
 shape(image)
 
@@ -13,7 +13,7 @@ shape(image)
     
 # onscreenclick(get_mouse_click_cor)
 
-data = pandas.read_csv(r'27_states.csv')
+data = pandas.read_csv('27_states.csv')
 english_list_data = data['english name'].to_list()
 arabic_list_data = data['arabic name'].to_list()
 text = Turtle()
@@ -22,7 +22,10 @@ text.penup()
 text.speed(0)
 score = 0
 while True:
-    answer = screen.textinput(title=f'{score}/27 States correct', prompt="What's the state name?\nor if you are done type 'exit'").title()
+    try:
+        answer = screen.textinput(title=f'{score}/27 States correct', prompt="What's the state name?\nor if you are done press Cancel").title()
+    except:
+        break
     if (answer in english_list_data):
         # A way to access element in csv file
         answer_index = english_list_data.index(answer)
@@ -40,14 +43,15 @@ while True:
         english_list_data[answer_index] = None
         arabic_list_data[answer_index] = None
         score += 1
-    elif answer == 'Exit':
-        break
 
 # states_to_learn = pandas.DataFrame()
 english_list_data = list(dict.fromkeys(english_list_data))
-english_list_data.remove(None)
 arabic_list_data = list(dict.fromkeys(arabic_list_data))
-arabic_list_data.remove(None)
+try:
+    english_list_data.remove(None)
+    arabic_list_data.remove(None)
+except:
+    pass
 missing_states = {'Arabic Name' : arabic_list_data, 'English Name' : english_list_data}
 data = pandas.DataFrame(missing_states)
-data.to_csv(r'missing_states.csv')
+data.to_csv('missing_states.csv')
